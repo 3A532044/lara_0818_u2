f160126beb86ac01113a557f90cef81a952eccd2
林宥丞 <a26913700@gmail.com>
2018-11-04 01:59:43 +0800

    練習2:建立樣板結構

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>@yield('title') | My Blog</title>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Monster
+ * Date: 2018/11/4
+ * Time: 上午 01:56
+ */
